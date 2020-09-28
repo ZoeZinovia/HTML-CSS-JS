0bc40e089021a0e741b5c7197ea992dc3fc86360
Zoe Zinovia <53115380+ZoeZinovia@users.noreply.github.com>
2020-09-28 14:48:05 +0100

    JQuery additions

--- app.js
+++ app.js
@@ -13,11 +13,53 @@ var btn = document.querySelector("button");
 var img = document.querySelector("img");
 var num = 1;
 
-btn.addEventListener("click", myFunction);
+$("#para1 span").css("text-decoration", "underline");
+// $("*").css("color", "red");
+
+// btn.addEventListener("click", myFunction);
+// $(document).ready(function(){
+//     alert("ready!");
+// })
+$("button").on("click", myFunction);
+// $(document).on("mousemove", myFunction4);
+// $("input").focus(function(){
+//     $(this).css('background', "gray");
+// });
+// $("input").blur(function(){
+//     $(this).css('background', "white");
+// });
+// $("#form").submit(function(e){
+//     var name = $("input").val();
+//     alert(name);
+// });
+// $("button").on("mousedown", myFunction2);
+// $("button").on("mouseup", myFunction2);
 
 function myFunction()
 {
-
     img.src = pics[num];
     num = (num+1)%8;
+}
+
+function myFunction2()
+{
+    $("#para1").toggle();
+}
+
+function myFunction3(e)
+{
+    alert(e.currentTarget.innerHTML);
+}
+
+function myFunction4(e)
+{
+    $("#coords").html("coords: Y: " + e.clientY + ", X: " + e.clientX);
+}
+
+// $("#para1 span").html("<button>Hello</button>");
+// $("#para1 span").append("<h6>Hello</h6>");
+
+function myFunction5()
+{
+    $("#submit").fadeOut();
 }
